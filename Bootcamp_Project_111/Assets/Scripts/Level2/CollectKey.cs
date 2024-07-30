75e3e36c38682f8a83ce123d32b429b25c76c760
Seyda Balli <marinaseyda7@gmail.com>
2024-07-30 14:48:27 +0300

    cinematic scene
    -merge conflict

---
+++ Bootcamp_Project_111/Assets/Scripts/Level2/CollectKey.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectKey : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        //nice sound
+        //new key! (player ui)
+        //game manager (key bool) true
+        //Destroy gameobject
+
+    }
+}
