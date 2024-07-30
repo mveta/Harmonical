b1a46a5e8dddd658186f464c94859ecb258c351a
Polatcan Gürbüz <427638@ogr.ktu.edu.tr>
2024-07-30 11:46:24 +0300

    boat movement and buttons canvas added
    - Botun hareket kodları yazıldı
    -  F butonunun ve U,I butonlarının canvasları hazırlandı
    - Player'ın yer değiştirme kodları yazıldı

--- Bootcamp_Project_111/Assets/Scripts/BoatMovement.cs
+++ Bootcamp_Project_111/Assets/Scripts/BoatMovement.cs
@@ -9,17 +9,37 @@ public class BoatMovement : MonoBehaviour
     
     [SerializeField] float speed=5f;
     [SerializeField] GameObject player, playerboat;
+    [SerializeField] GameObject fcanvas,uibuttoncanvas;
+     int count = 0;
+  
+   
     void Update()
     {
         if (Input.GetKey(KeyCode.U))
         {
             transform.Rotate(new Vector3(0,-5f,0)*speed*Time.deltaTime);
-           // playerboat.transform.Rotate(new Vector3(0, -5f, 0) * speed * Time.deltaTime);
+           
         } 
         if(Input.GetKey(KeyCode.I)) {
             transform.Rotate(new Vector3(0, 5f, 0) *speed* Time.deltaTime);
-           // playerboat.transform.Rotate(new Vector3(0, -5f, 0) * speed * Time.deltaTime);
+           
         }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            player.SetActive(false);
+            playerboat.SetActive(true);
+            fcanvas.SetActive(false);
+        }
+        if(playerboat.active == true)
+        {
+            transform.Translate(new Vector3(0, 0, -3f) * Time.deltaTime );
+            uibuttoncanvas.SetActive(true);
+        }
+        else
+        {
+            uibuttoncanvas.SetActive(false);
+        }
+        
     }
 
    
@@ -27,8 +47,13 @@ public class BoatMovement : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            player.SetActive(false);
-            playerboat.SetActive(true);
+            count++;
+            if(count == 1)
+            fcanvas.SetActive(true);
+            
+            
+           
+           
         }
     }
 
